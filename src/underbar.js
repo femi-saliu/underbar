cb97be1b6375886550aeafc0efe44cc76969347b
Femi Saliu <femisaliu.gmail.com>
2015-06-01 18:09:18 -0700

    Implemented _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -89,8 +89,13 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var result = [];
+    for (var i=0; i<collection.length; i++){
+      if (test(collection[i])===false){
+        result.push(collection[i]);
+      }
+    }
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
